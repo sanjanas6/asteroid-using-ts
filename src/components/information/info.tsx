e595e9642ea38bb36a95b0ad950316721759fcf0
Sanjana Singh <sanjanasingh7774@gmail.com>
2022-10-06 15:51:59 +0530

    Changes Done

--- src/components/information/info.tsx
+++ src/components/information/info.tsx
@@ -4,6 +4,16 @@ import { useLocation } from "react-router-dom";
 type Asteroid = {
   id: number;
   name: string;
+  name_limited: string;
+  designation: number;
+  estimated_diameter: {
+    feet: {
+      estimated_diameter_max: number;
+    }
+  }
+  orbital_data: {
+    orbit_id: number;
+  }
 };
 
 const Information = () => {
@@ -14,8 +24,12 @@ const Information = () => {
 
   return (
     <div className="information" data-testid="divmain">
-      <h2 style={{textAlign: "center" , color:"Blue"}}>Asteroid id: {data.id}</h2>
-      <h3 style={{textAlign: "center" , color:"Blue"}}>Asteroid name: {data.name}</h3>
+      <h2 style={{textAlign: "center" , color:"Blue"}}>Asteroid id: {data?.id}</h2>
+      <h3 style={{textAlign: "center" , color:"Blue"}}>Asteroid name: {data?.name}</h3>
+      <h3 style={{textAlign: "center" , color:"Blue"}}>Asteroid Limited name: {data?.name_limited}</h3>
+      <h3 style={{textAlign: "center" , color:"Blue"}}>Asteroid Designation: {data?.designation}</h3>
+      <h3 style={{textAlign: "center" , color:"Blue"}}>Max. Estimated Diameter: {data?.estimated_diameter.feet.estimated_diameter_max}</h3>
+      <h3 style={{textAlign: "center" , color:"Blue"}}>Orbit Id: {data?.orbital_data.orbit_id}</h3>
     </div>
   );
 };
